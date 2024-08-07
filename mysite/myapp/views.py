ead1fe19d488baae373c9fde42794e04ff7d662f
iamluwin <luwin.anuka@gmail.com>
2024-08-07 23:58:18 +0530

    added template

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Products
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world")
 
 
 def products(request):
-    return HttpResponse("List of Products")
+    products = Products.objects.all()
+    context ={
+        'products': products
+    }
+    return render(request, 'myapp/index.html', context)
