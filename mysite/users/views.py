2fb02370699ecf34e18921d526a7b7fa30b497a8
iamluwin <luwin.anuka@gmail.com>
2024-08-16 01:07:54 +0530

    Register process backend configuration

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import newUserForm
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = newUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('myapp/products')
+
+    form = newUserForm()
+    context={
+        'form': form,
+    }
+
+    return render(request,'users/register.html', context)
