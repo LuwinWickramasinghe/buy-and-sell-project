d274d68d9e1f6bbe99b68126d8c7f83001330d55
iamluwin <luwin.anuka@gmail.com>
2024-08-21 00:03:18 +0530

    Create Profile View and url handle

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -20,3 +20,7 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+def create_profile(request):
+    return render(request, 'users/createprofile.html')
