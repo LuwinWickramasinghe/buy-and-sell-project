7cb15258c169d4c01d65078c0ed13b639729006d
iamluwin <luwin.anuka@gmail.com>
2024-08-21 23:59:51 +0530

    customize admin panel - add display fields, search bar

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Products
 # Register your models here.
 
-admin.site.register(Products)
+
+admin.site.site_header = "SnapTrade"
+admin.site.site_title = "SnapTrade Admin"
+admin.site.index_title = "Manage SnapTrade"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'desc')
+    search_fields = ('name',)
+    
+
+
+admin.site.register(Products,ProductAdmin)
