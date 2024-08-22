da75b6e9b7a64aeb19c078c62fcc3efbe878c26a
iamluwin <luwin.anuka@gmail.com>
2024-08-23 02:10:31 +0530

    adding functionality of admin panel

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -10,7 +10,15 @@ admin.site.index_title = "Manage SnapTrade"
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'desc')
     search_fields = ('name',)
-    
+
+
+
+    def set_price_to_zero(self,request,queryset):
+        queryset.update(price=0)
+
+    actions = ('set_price_to_zero',)
+    list_editable = ('price','desc')
+
 
 
 admin.site.register(Products,ProductAdmin)
